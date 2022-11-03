0aa5dec29201a99be34e54ebfd5039eafbed1bcd
Rafael Ribeiro <rafael.ntw@gmail.com>
2022-11-03 18:34:58 +0000

    feat: use python script as shell script with python interpreter

--- hooks/techdocs-replace-index.py
+++ hooks/techdocs-replace-index.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 import os
-def main():
-    os.system('cp readme.md docs/index.md')
-    print("Copying the File")
-    textToSearch = "(docs/images/"
-    textToReplace = "(images/"
-    docs_file="docs/index.md"
-    # Read in the file
-    with open(docs_file, 'r') as file :
-        filedata = file.read()
+    
+os.system('cp ./readme.md ./docs/index.md')
+print("Copying the File")
+textToSearch = "(docs/images/"
+textToReplace = "(images/"
+destination_file="docs/index.md"
+# Read in the file
+with open(destination_file, 'r') as file :
+    filedata = file.read()
 
-    # Replace the target string
-    filedata = filedata.replace(textToSearch, textToReplace)
+# Replace the target string
+filedata = filedata.replace(textToSearch, textToReplace)
 
-    # Write the file out again
-    with open(docs_file, 'w') as file:
-        file.write(filedata)
-        
-    print("Replaced paths on readme for techdocs")
+# Write the file out again
+with open(destination_file, 'w') as file:
+    file.write(filedata)
+    
+print("Replaced paths on readme for techdocs")
