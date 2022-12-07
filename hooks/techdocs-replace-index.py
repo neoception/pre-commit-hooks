974d73f7a88a633ade05e23cc67581c278fa51d1
Rafael Ribeiro <rafael.ntw@gmail.com>
2022-12-07 10:45:49 +0000

    fix: copy all readme files

--- hooks/techdocs-replace-index.py
+++ hooks/techdocs-replace-index.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
+from pathlib import Path
 import os
-    
-os.system('cp ./readme.md ./docs/index.md')
+import shutil
+
+dir_src = "./"
+
+for filename in os.listdir("."):
+    if filename.lower().startswith("readme"):
+        shutil.copyfile(dir_src + filename, "./docs/index.md")
+
 print("Copying the File")
 textToSearch = "(docs/images/"
 textToReplace = "(images/"
-destination_file="docs/index.md"
+destination_file = "docs/index.md"
 # Read in the file
-with open(destination_file, 'r') as file :
+with open(destination_file, "r") as file:
     filedata = file.read()
 
 # Replace the target string
 filedata = filedata.replace(textToSearch, textToReplace)
 
 # Write the file out again
-with open(destination_file, 'w') as file:
+with open(destination_file, "w") as file:
     file.write(filedata)
-    
+
 print("Replaced paths on readme for techdocs")
